60826b5943d236d9a0987099139319ae9cd0b583
Daniel-Paul-chukwudi <paulchukwudidaniel@gmail.com>
2026-04-15 11:43:16 +0200

    first push

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const PORT = process.env.PORT || 1234
 const DB = process.env.DB
-// const cors = require('cors')
+const cors = require('cors')
 // const swaggerJSDoc = require('swagger-jsdoc')
 // const swaggerUi  = require('swagger-ui-express')
 const mongoose = require("mongoose")
@@ -12,21 +12,24 @@ const userRouter = require('./routes/userRoutes')
 
 const app = express()
 app.use(express.json())
-// app.use(cors())
+app.use(cors())
 app.use(surveyRouter)
 app.use(userRouter)
 
+app.listen(PORT, () => {
+  console.log('Server is running on Port:', PORT)
+})
 
 
-const Startserver = async ()=>{ 
-  mongoose.connect(DB).then(() => {
-    console.log('Connected to Database')
-    app.listen(PORT, () => {
-      console.log('Server is running on Port:', PORT)
-    })
-  }).catch((error) => {
-    console.log('Error connecting to Database', error.message)
-  });
-};
+// const Startserver = async ()=>{ 
+//   mongoose.connect(DB).then(() => {
+//     console.log('Connected to Database')
+//     app.listen(PORT, () => {
+//       console.log('Server is running on Port:', PORT)
+//     })
+//   }).catch((error) => {
+//     console.log('Error connecting to Database', error.message)
+//   });
+// };
 
-Startserver();
+// Startserver();
